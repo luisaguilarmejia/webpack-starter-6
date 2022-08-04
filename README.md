a7034e38c66588545ea901c4388447cea4a18be5
LUIS <luise.aguilarmejia@gmail.com>
2022-08-04 01:00:01 -0400

    Primer commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Webpack Starter-5
+
+Este es el proyecto inicial para crear aplicaciones utilizando webpack - 5
+
+### Notas:
+Recuerden reconstruir los módulos de Node
+```
+npm install
+```
+
+...Y para construir el build, recueden correr : 
+
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin    = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin           = require("copy-webpack-plugin");
+
+
+module.exports = {
+    // desarrollo 
+    mode: 'development',
+
+    output: {
+        clean: true,
+    },
+
+    module:{
+        rules: [
+            {
+                test   : /\.html$/,
+                loader : 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test   : /\.css$/,
+                exclude: /styles.css$/,
+                use    : ['style-loader', 'css-loader'],
+            },
+            {
+                test   : /styles.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+            },
+        ]
+    },
+
+    optimization: {},
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Mi Webpack App',
+            // filename: 'index.html' template es donde queremos que se base 
+            template: './src/index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename   :   '[name].css',
+            ignoreOrder: false,
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/assets/', to: 'assets/'},
+            ]
+        }),
+
+    ],
+}
